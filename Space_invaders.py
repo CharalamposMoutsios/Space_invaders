2ca85911d0d330cf275ef0803cf8cc8e273a3fe9
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-30 09:14:49 +0200

    updated

--- Space_invaders.py
+++ Space_invaders.py
@@ -13,3 +13,14 @@ playerX_change = 0
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+# Set up the enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 4
+enemyY_change = 40
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
