772c4a0b483ebf3b3d439ee3b4a56038dc4cf00a
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-30 09:14:21 +0200

    lets start

--- Space_invaders.py
+++ Space_invaders.py
@@ -4,3 +4,12 @@ pygame.init()
 # Set up the screen
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invaders")
+
+# Set up the player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
